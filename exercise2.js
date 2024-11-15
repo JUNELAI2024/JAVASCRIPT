221ca2b106717fa1b340f631a93f50bf2662a212
JUNELAI2024 <159815232+JUNELAI2024@users.noreply.github.com>
2024-11-15 08:34:32 +0800

    e2
    e2

---
+++ exercise2.js
@@ -0,0 +1,9 @@
+const select = document.querySelector("select")
+const para = document.querySelector("p")
+select.addEventListener("change", setWeather)
+function setWeather() {
+const choice = select.value
+// Write the solution between these lines
+// =================================
+// =================================
+}
