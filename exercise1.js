476861b085344e12314acb2612de80e50a59949f
JUNELAI2024 <159815232+JUNELAI2024@users.noreply.github.com>
2024-11-21 07:47:25 +0800

    assignment2
    ass2

--- exercise1.js
+++ exercise1.js
@@ -1,15 +1,23 @@
+function lastLetter(str) {
+    return str[str.length - 1]; // Return the last character
+}
+
+// Example usage
+console.log(lastLetter("helloworld")); // Output: "d"
+
+
 // hello.js 
-console.log('Hello World!')
+//console.log('Hello World!')
 //Declare constant
-const MAX = '57'
+//const MAX = '57'
 
 //Declare variable 
-let actual;
-actual = MAX -13;
-let percentage;
-percentage = actual/MAX;
+//let actual;
+//actual = MAX -13;
+//let percentage;
+//percentage = actual/MAX;
 // Round the percentage to two decimal places
-let roundedPercentage = percentage.toFixed(2); // No need to multiply by 100 for this case
+//let roundedPercentage = percentage.toFixed(2); // No need to multiply by 100 for this case
 
 // Log the output to the console
-console.log('The percentage is ${roundedPercentage}');
+//console.log('The percentage is ${roundedPercentage}');
